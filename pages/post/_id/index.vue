8e67c4699be6dda6da1a3eb27c3b4b52b03d80be
ryuhei4k9 <ryuhei.4k9@gmail.com>
2018-11-27 23:19:27 +0900

    個別記事読み込み

--- pages/post/_id/index.vue
+++ pages/post/_id/index.vue
@@ -4,37 +4,40 @@
       <h2 class="title is-2">{{ post.title }}</h2>
       <div v-html="post.content"></div>
       <br>
-      <h4 class="title is-5 is-marginless">by <strong>{{ post.author }}</strong> at <strong>{{ post.published }}</strong></h4>
+      <h4 class="title is-5 is-marginless">by <strong>{{ post.createdAt }}</strong> at <strong>{{ post.createdAt }}</strong></h4>
     </div>
   </div>
 </template>
 
 <script>
-  // import posts saved JSON data
-  import posts from '~/posts.json'
+  import post from '~/apollo/queries/post'
 
   export default {
-    validate ({ params }) {
-      return /^\d+$/.test(params.id)
-    },
-    asyncData ({ params }, callback) {
-      let post = posts.find(post => post.id === parseInt(params.id))
-      if (post) {
-        callback(null, { post })
-      } else {
-        callback({ statusCode: 404, message: 'Post not found' })
+    // validate ({ params }) {
+    //   return /^\d+$/.test(params.id)
+    // },
+    name: 'post',
+    // asyncData ({ params }, callback) {
+    //   let post = posts.find(post => post.id === parseInt(params.id))
+    //   if (post) {
+    //     callback(null, { post })
+    //   } else {
+    //     callback({ statusCode: 404, message: 'Post not found' })
+    //   }
+    // },
+    data () {
+      return {
+        post: {}
       }
     },
-    head () {
-      return {
-        title: this.post.title,
-        meta: [
-          {
-            hid: 'description',
-            name: 'description',
-            content: this.post.summary
+    apollo: {
+      post: {
+        query: post,
+        variables() {
+          return {
+            id: this.$route.params.id
           }
-        ]
+        }
       }
     }
   }
